67b3ca51466be85ab583fe92e559fdaae32c6487
Jonathan Spira <jjspira@gmail.com>
2020-09-06 23:03:25 -0700

    And remove the config

--- src/config.ts
+++
@@ -1,3 +0,0 @@
-export const YY_BOSS_EXE = 'C:/Users/jjspi/Documents/Projects/Rust/yy-boss/target/release/yy-boss-cli.exe';
-
-export const WD = 'C:\\Users\\jjspi\\Documents\\Projects\\Typescript\\gm-code-vsc\\tmp';
